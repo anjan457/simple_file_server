1c5f6daf7fdf9a688a48d6f3dc79ef75b1d3c337
anjan457 <anjankundu392@gmail.com>
2023-05-02 20:52:25 +0600

    file server

---
+++ main.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+const publicDir = path.join(__dirname, 'public');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(publicDir, req.url === '/' ? 'index.html' : req.url);
+  const extname = path.extname(filePath);
+  const contentType = getContentType(extname);
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404);
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(500);
+        res.end('Internal Server Error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+function getContentType(extname) {
+  switch (extname) {
+    case '.html':
+      return 'text/html';
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'text/javascript';
+    default:
+      return 'application/octet-stream';
+  }
+}
